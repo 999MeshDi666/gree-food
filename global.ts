6846b641ed8f65b1fc115ace16e98a3471da3a28
madi <havkpzz@gmail.com>
2023-10-30 14:03:47 +0600

    Add prettier and eslint

---
+++ global.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const global = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    paddingHorizontal: 15,
+    backgroundColor: '#3C753E',
+  },
+  text: {
+    color: '#fff',
+  },
+});
