82e059dde5ddbb74132867377695e9e46541b8a5
Madi <havkpzz@gmail.com>
2023-11-11 13:08:39 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { global } from '../global';
 
 export default function App() {
   return (
     <View style={global.container}>
-      <Text style={global.text}>Open up App.tsx to start!</Text>
+      <Text style={global.text}>Open up App.tsx to start</Text>
       <StatusBar />
     </View>
   );
